0f7a3067b97055d8f0dc400e8ed84419a9244511
Anshul Chauhan <anshulchauhan@outlook.com>
2016-03-27 04:00:16 +0530

    Added submit button and gave it functionality

--- app/src/main/java/com/anshulchauhan/nobunk/MainActivity.java
+++ app/src/main/java/com/anshulchauhan/nobunk/MainActivity.java
@@ -3,13 +3,16 @@ package com.anshulchauhan.nobunk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import java.io.File;
 import android.widget.Toast;
 
+import java.io.FileOutputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,12 +20,20 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = "pokemon";
+    ListView theStudentListView;
+    File attendanceFile;
+    String filename = "attendance.txt";
+    FileOutputStream outputStream;
+
     public ArrayAdapter<String> studentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        attendanceFile = new File(getBaseContext().getFilesDir(),filename);
 
         String[] studentsList = {
                 "011 - Anshul Chauhan",
@@ -48,12 +59,15 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice,
                 studentsList);
 
-        ListView theStudentListView = (ListView) findViewById(R.id.listViewOfStudents);
+        theStudentListView = (ListView) findViewById(R.id.listViewOfStudents);
 
         theStudentListView.setAdapter(theAdapter);
+
+        // set the selection choice to multiple
         theStudentListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
         theStudentListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             /*
              * Parameters
              * adapterView - where the click happened
@@ -68,6 +82,20 @@ public class MainActivity extends AppCompatActivity {
                 //Toast.makeText(MainActivity.this, studentPicked, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void submitButtonClicked(View view) {
+        Log.i(TAG, "submit button clicked");
+        SparseBooleanArray checkedItems = theStudentListView.getCheckedItemPositions();
+        if (checkedItems != null) {
+            for (int i=0; i<checkedItems.size(); i++) {
+                if (checkedItems.valueAt(i)) {
+                    String item = theStudentListView.getAdapter().getItem(
+                                          checkedItems.keyAt(i)).toString();
+                    Log.i(TAG,item + " was selected");
+                }
+            }
+        }
     }
 
 }
